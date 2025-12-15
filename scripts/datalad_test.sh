e27a15629e14e73936b7151efc1925133f565452
Russell Poldrack <poldrack@gmail.com>
2025-12-15 14:16:10 -0800

    initial add

---
+++ scripts/datalad_test.sh
@@ -0,0 +1,7 @@
+datalad create -d . my_datalad_repo
+datalad download-url -d . -O my_datalad_repo/data/ https://raw.githubusercontent.com/IanEisenberg/Self_Regulation_Ontology/refs/heads/master/Data/Complete_02-16-2019/demographics.csv
+datalad download-url -d . -O my_datalad_repo/data/ https://raw.githubusercontent.com/IanEisenberg/Self_Regulation_Ontology/refs/heads/master/Data/Complete_02-16-2019/meaningful_variables_clean.csv
+datalad unlock my_datalad_repo/data/demographics.csv
+python src/BetterCodeBetterScience/modify_data.py my_datalad_repo/data/demographics.csv
+datalad save -d . -m "Modified demographics.csv" my_datalad_repo/data/demographics.csv
+datalad status
